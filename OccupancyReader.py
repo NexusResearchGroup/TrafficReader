5b5663a88a523c5f9df442f053744c7caa119dd4
owenx148 <owenx148@umn.edu>
2012-03-14 14:07:17 -0500

    Got rid of Eclipse mess; that was a bad idea for a project of this scope.

---
+++ OccupancyReader.py
@@ -0,0 +1,22 @@
+'''
+Created on Mar 8, 2012
+
+@author: owenx148
+'''
+
+class OccupancyReader():
+    '''
+    Reads occupancy data from the occupancy file within a .traffic file
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+
+if __name__ == '__main__':
--- TrafficReader/src/TrafficReader/__init__.py
+++
@@ -1,5 +0,0 @@
-import OccupancyReader
-import VolumeReader
-
-class TrafficReader:
-    
