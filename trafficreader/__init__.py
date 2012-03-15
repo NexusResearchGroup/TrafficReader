e1479edc0ed7a2970d0fec6861a418a2110a2088
owenam <owenam@gmail.com>
2012-03-15 16:09:46 -0500

    class skeleton for trafficreader

--- trafficreader/__init__.py
+++ trafficreader/__init__.py
@@ -1,1 +1,13 @@
 from readers import list_occupancies, list_volumes
+
+class TrafficReader:
+	'''
+	Provides an interface to a single .traffic file
+	'''
+	
+	def __init__(self, trafficfile=None):
+		'''
+		Returns a new TrafficReader, optionally initialized with a specified .traffic file
+		'''
+		
+	
