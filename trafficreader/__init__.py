d8ebb85e26baa652e1a004d88b18bd82d50a856b
owenam <owenam@gmail.com>
2012-03-19 10:52:57 -0500

    Defined methods for TrafficReader class

--- trafficreader/__init__.py
+++ trafficreader/__init__.py
@@ -9,5 +9,22 @@ class TrafficReader:
 		'''
 		Returns a new TrafficReader, optionally initialized with a specified .traffic file
 		'''
+	
+	def loadfile(
+	
+	def list_detectors(self):
+		'''
+		Returns a list of the IDs of all detectors who have records in the current .traffic file
+		'''
+	
+	def occupancies_for_detector(self, detectorID):
+		'''
+		Returns a list of the occupancy values recorded in this .traffic file for the detector with the specified ID.
+		'''
 		
+	def volumes_for_detector(self, detectorID):
+		'''
+		Returns a list of the volume values recorded in this .traffic file for the detector with the specified ID.
+		'''
+	
 	
