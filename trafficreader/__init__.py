01300aa7bd187763df496b4c6e899dab2c058288
owenam <aowen@owenam-pc.(none)>
2012-03-25 19:58:39 -0500

    - Implemented 1m_volumes_for_detector() and 1m_occupancies_for_detector()

--- trafficreader/__init__.py
+++ trafficreader/__init__.py
@@ -45,7 +45,7 @@ class TrafficReader:
 	
 	def occupancies_for_detector(self, detectorID):
 		'''
-		Returns a list of the occupancy values recorded in this .traffic file for the detector with the specified ID.
+		Returns a list of the 30-second occupancy values recorded in this .traffic file for the detector with the specified ID.
 		'''
 		
 		name = str(detectorID) + '.c30'
@@ -55,9 +55,43 @@ class TrafficReader:
 		
 	def volumes_for_detector(self, detectorID):
 		'''
-		Returns a list of the volume values recorded in this .traffic file for the detector with the specified ID.
+		Returns a list of the 30-second volume values recorded in this .traffic file for the detector with the specified ID.
 		'''
 		
 		name = str(detectorID) + '.v30'
 		vol_file = self._zipfile.open(name)
 		return list_volumes(vol_file)
+	
+	def 1m_volumes_for_detector(detectorID):
+		'''
+		Returns a list of 1-minute volume values, one for each minute of the day, starting at 00:00. If both applicable 30-second volumes are valid, they are added together. If one or both 30-second volumes are missing, -1 is reported for that time slot. 
+		'''
+		
+		30s_volumes = self.volumes_for_detector(detectorID)
+		1m_volumes = []
+		
+		for i in range(0, len(30s_volumes), 2):
+			if 30s_volumes[i] == -1 or 30s_volumes[i+1] == -1:
+				1m_volumes.append(-1)
+			else:
+				1m_volumes.append(30s_volumes[i] + 30s_volumes[i+1])
+		
+		return 1m_volumes
+		
+	def 1m_occupancies_for_detector(detectorID):
+		'''
+		Returns a list of 1-minute occupancy ratios, one for each minute of the day, starting at 00:00. If both applicable 30-second occupancies are valud, they are averaged together. If one or both 30-second occupancies are missing, -1 is reported for that time slow.
+		'''
+		
+		30s_occupancies = self.occupancies_for_detector(detectorID)
+		1m_occupancies = []
+		
+		for i in range(0, len(30s_occupancies), 2):
+			if 30s_occupancies[i] == -1 or 30s_occupancies[i+1] == -1:
+				1m_occupancies.append(-1)
+			else:
+				1m_occupancies.append((30s_occupancies[i] + 30s_occupancies[i+1]) / 2)
+	
+
+		
+		
