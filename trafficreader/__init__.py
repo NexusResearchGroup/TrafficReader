381d5a89fdd3d3edd4a2f1bc680dba0c895baaa5
owenam <aowen@owenam-pc.(none)>
2012-03-25 22:35:52 -0500

    - implemented onemin_speeds_for_detector()

--- trafficreader/__init__.py
+++ trafficreader/__init__.py
@@ -92,6 +92,59 @@ class TrafficReader:
 			else:
 				1m_occupancies.append((30s_occupancies[i] + 30s_occupancies[i+1]) / 2)
 	
+	def onemin_speeds_for_detector(self, detectorID, speed_limit=70):
+		'''
+		Returns a list of 1-minute speeds, one for each minute of the day, starting at 00:00.
+		'''
+		
+		vols = self.onemin_volumes_for_detector(detectorID)
+		occs = self.onemin_occupancies_for_detector(detectorID)
+		avg_field_length, field_lengths = self.field_lengths(vols, occs, speed_limit)
+		free_flow_speed = self.free_flow_speed(vols, occs, avg_field_length)
+		
+		speeds = []
+		
+		# given in published report
+		theta = 0.15
+		
+		for i in range(len(occs)):
+			if occs[i] < 0:
+				speeds.append(-1)
+			elif 0 < occs[i] < 0.1:
+				speeds.append(free_flow_speed * (1 - ( (occs[i] * avg_field_length) / field_lengths[i]) ) )
+			elif 0.1 <= occs[i] <= 0.15:
+				speeds.append(free_flow_speed * (1 - occs[i]) )
+			else:
+				exponent = -1 * (1 / theta) * ((100 * occs[i]) / (100 - theta))
+				speeds.append(free_flow_speed * (1 - theta) * exp(exponent) )
+		
+		return speeds
+				
+	def field_lengths(self, volumes, occupancies, speed_limit=70):
+		'''
+		Given a list of volumes, a list of corresponding occupancies, and a speed limit, returns overall average effective field length of the detector.
+		'''
+		
+		lengths = []
+		
+		for i in range(len(volumes)):
+			if 0 <= occupancies[i] < 0.1:
+				if volumes[i] == 0 or occupancies[i] == 0:
+					lengths.append(-1)
+				elif occupancies[i] >= 0.1:
+					lengths.append(-2)
+				else:
+					lengths.append( (speed_limit * occupancies[i] * 5280) / (volumes[i] * 60) )
+			else:
+				lengths.append(-1)
+		
+		valid_lengths = []
+		
+		for i in range(len(lengths)):
+			if not (lengths[i] == -1 or lengths[i] == -2):
+				valid_lengths.append(lengths[i])
+		
+		average_length = sum(valid_lengths) / len(valid_lengths)
 
 		
 		
